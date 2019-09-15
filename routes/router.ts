98695ecaacfbc89ae317a69c76a16b7a7e76e5be
Andres Alba <bryanalba92@gmail.com>
2019-09-15 18:31:15 -0500

    Enviar un mensaje al chat general desde un servicio REST

--- routes/router.ts
+++ routes/router.ts
@@ -15,6 +15,16 @@ router.get('/mensajes', ( req: Request, res: Response ) => {
 router.post('/mensajes', ( req: Request, res: Response ) => {
     const cuerpo = req.body.cuerpo;
     const de = req.body.de;
+
+    const payload = {
+        de,
+        cuerpo
+    }
+
+    const server = Server.instance;
+
+    server.io.emit('mensaje-nuevo',payload);
+
     res.json({
         ok: true,
         cuerpo,
