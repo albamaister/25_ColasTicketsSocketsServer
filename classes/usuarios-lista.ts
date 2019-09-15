c9c7dff37196877875e95c45093b88444dbc8ad8
Andres Alba <bryanalba92@gmail.com>
2019-09-14 23:22:08 -0500

    Configuración básica de un usuario en el lado del servidor

---
+++ classes/usuarios-lista.ts
@@ -0,0 +1,55 @@
+import { Usuario } from './usuario';
+// Tener centralizado la logica de todos mis usuarios, procesos para agregar, proceosos para mandar un mensaje 
+
+export class UsuariosLista {
+    private lista: Usuario[] = [];
+
+    constructor() {}
+
+    // agregar un usuario
+    public agregar( usuario: Usuario ) {
+        this.lista.push(usuario);
+        console.log(this.lista);
+        return usuario;
+    }
+
+    public actualizarNombre( id: string, nombre: string ) {
+        for( let usuario of this.lista ) {
+            if ( usuario.id === id ) {
+                usuario.nombre = nombre;
+                break;
+            }
+        }
+        console.log('==================actualizando usuario================');
+        console.log(this.lista);
+    }
+
+
+    // Obtener lista de usuarios
+    public getLista() {
+        return this.lista;
+    }
+    
+
+    // Obtener un usuario
+    public getUsuario( id: string ) {
+        this.lista.find( usuario => {
+            return usuario.id === id;
+        } );
+    }
+
+    // Obtener usuarios en una sala en particular
+    public getUsuariosSala( sala: string ) {
+        return this.lista.filter( usuario => usuario.sala === sala )
+    }
+
+    // Borra un usuario
+    public borrarusuario( id: string ) {
+        const tempusuario = this.getUsuario(id);
+
+        this.lista = this.lista.filter( usuario => usuario.id !== id );
+        return tempusuario;
+    }
+
+}
+
